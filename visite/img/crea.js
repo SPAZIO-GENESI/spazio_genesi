26cea390bd4850d08c36eb54f8eb40f438976666
maxAlone <mascam64@gmail.com>
2024-10-08 12:38:47 +0200

    auto dyn 1

---
+++ visite/img/crea.js
@@ -0,0 +1,39 @@
+import { lista } from './lista.js';
+
+function caricalista(lista){
+    let immmagini[];
+    let descrizioni[];
+    let crediti[];
+
+    let quante = lista.length;
+    for (let x=0; x<quante; x++) {
+        immmagini.push(lista(0),x);
+        descrizioni.push(lista(1),x);
+        crediti.push(lista(2),x);
+    }
+    loadImages();
+}
+
+function loadImages() {
+    const imageFolder = './'; // Cartella delle immagini
+    const imageExtensions = ['jpg', 'jpeg', 'png', 'gif']; // Estensioni delle immagini
+    const carouselInner = document.getElementById('carousel-inner');
+
+    // Lista delle immagini (in un vero scenario, questa lista potrebbe essere generata dinamicamente)
+    const images = immmagini; //['accademiasanluca_benedettibiocca_gbartolomei.jpeg', 'accademiasanluca_bernini.jpeg', 'accademiasanluca_canova.jpeg']; // Sostituisci con i nomi delle tue immagini
+
+    images.forEach((image, index) => {
+        const div = document.createElement('div');
+        div.className = 'carousel-item' + (index === 0 ? ' active' : '');
+        const img = document.createElement('img');
+        img.src = imageFolder + image;
+        // img.className = 'd-block 100-vh ';
+        img.style = "max-height: 500px; max-width: 500px; height: auto; width: auto;";
+        img.alt = 'Slide ' + (index + 1);
+        div.appendChild(img);
+        carouselInner.appendChild(div);
+    });
+}
+
+// Carica le immagini quando la pagina è pronta
+document.addEventListener('DOMContentLoaded', caricalista);
---
+++ visite/img/lista.js
@@ -0,0 +1,6 @@
+export const lista = () => {
+    return ['accademiasanluca_canzianicamoni.jpeg','accademiasanluca_bernini.jpeg'],
+            ['di Benedetti / Biocca','di Canova'],
+            ['foto di Giulia Bartolomei','foto di <a href="https://www.instagram.com/nevoanarua">névoanarua</a>'];
+
+}
