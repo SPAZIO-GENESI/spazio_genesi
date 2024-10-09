85f0f178e97277e48a0766ef499b176b5234a9b2
maxAlone <mascam64@gmail.com>
2024-10-09 14:15:04 +0200

    change exp

--- visite/img/lista.mjs
+++ visite/img/lista.mjs
@@ -1,6 +1,8 @@
-export const listaa = () => { 
+const listaa = () => { 
     const listar = [['accademiasanluca_canzianicamoni.jpeg','accademiasanluca_bernini.jpeg'],
             ['di Benedetti / Biocca','di Canova'],
             ['foto di Giulia Bartolomei','foto di <a href="https://www.instagram.com/nevoanarua">névoanarua</a>']];
     return listar;
         }
+
+export {listaa};
