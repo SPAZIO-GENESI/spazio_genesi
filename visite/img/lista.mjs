535970523ff5d92f11831dddf5f6ffd1c660322f
maxAlone <mascam64@gmail.com>
2024-10-09 14:07:25 +0200

    listar

--- visite/img/lista.mjs
+++ visite/img/lista.mjs
@@ -1,4 +1,6 @@
-const listaa = [['accademiasanluca_canzianicamoni.jpeg','accademiasanluca_bernini.jpeg'],
+export const listaa = () => { 
+    const listar = [['accademiasanluca_canzianicamoni.jpeg','accademiasanluca_bernini.jpeg'],
             ['di Benedetti / Biocca','di Canova'],
             ['foto di Giulia Bartolomei','foto di <a href="https://www.instagram.com/nevoanarua">névoanarua</a>']];
-exports = listaa;
+    return listar;
+        }
