3e7a1c6e6d59fe3bb4bc61e25d8cf63743ce771d
maxAlone <mascam64@gmail.com>
2024-10-09 14:53:08 +0200

    stup sintx

--- visite/img/crea.js
+++ visite/img/crea.js
@@ -17,11 +17,11 @@ console.log("i="+quanti);
 function caricalista(lista){
     for (let x=0; x<quanto; x++) {
         immmagini.push(lista[0,x]);
-            console.log (immmagini(x));
+            console.log (immmagini[x]);
         descrizioni.push(lista[1,x]);
-            console.log (descrizioni(x));
+            console.log (descrizioni[x]);
         crediti.push(lista[2,x]);
-            console.log (crediti(x));
+            console.log (crediti[x]);
     }
     loadImages(immmagini, descrizioni, crediti);
 }
