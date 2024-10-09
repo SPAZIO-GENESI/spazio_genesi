2b0b4e6b18d76910b5862c16962be557eefa7f63
maxAlone <mascam64@gmail.com>
2024-10-09 16:03:29 +0200

    attribuzione

--- visite/img/lista.mjs
+++ visite/img/lista.mjs
@@ -1,7 +1,7 @@
 const listaa = () => { 
     const listar = [
             ['accademiasanluca_licatabellantoni.jpeg','accademiasanluca_canzianicamoni.jpeg','accademiasanluca_bernini.jpeg'],
-            ['A cura di Maria Licata - Elena Bellantoni, the banality of evil in 22 gestures','di Benedetti / Biocca','di Canova'],
+            ['A cura di Maria Licata - Elena Bellantoni, the banality of evil in 22 gestures','di Benedetti / Biocca','Terracotta di Gian Lorenzo Bernini'],
             ['foto di névoanarua','foto di Giulia Bartolomei','foto di névoanarua']]
             ;
     return listar;
