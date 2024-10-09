fd32cdedb5ee756572d290e8da3f52870ca20329
maxAlone <mascam64@gmail.com>
2024-10-09 15:57:54 +0200

    add img

--- visite/img/lista.mjs
+++ visite/img/lista.mjs
@@ -1,8 +1,8 @@
 const listaa = () => { 
     const listar = [
-            ['accademiasanluca_canzianicamoni.jpeg','accademiasanluca_bernini.jpeg'],
-            ['di Benedetti / Biocca','di Canova'],
-            ['foto di Giulia Bartolomei','foto di <a href="https://www.instagram.com/nevoanarua">névoanarua</a>']]
+            ['accademiasanluca_licatabellantoni.jpeg','accademiasanluca_canzianicamoni.jpeg','accademiasanluca_bernini.jpeg'],
+            ['A cura di Maria Licata - Elena Bellantoni, the banality of evil in 22 gestures','di Benedetti / Biocca','di Canova'],
+            ['foto di névoanarua','foto di Giulia Bartolomei','foto di névoanarua']]
             ;
     return listar;
         };
