64702b08d21a1294e126ab877f940f2072ad29bb
maxAlone <mascam64@gmail.com>
2024-11-03 10:48:50 +0100

    qr 1

--- script.js
+++ script.js
@@ -8,6 +8,16 @@ function shortu(){
     }
   }
 
+  function qrcode(){
+    quales = document.getElementById("link1").value;
+    if (quales){
+      urls = JSON.parse(Get("https://sentinelle.mappa.asud.net/qr?qrcodeinput="+quales));
+      console.log(urls);
+      link1 = document.getElementById("link1");
+      link1.value =urls.secureShortURL;
+    }
+  }
+
   function Get(yourUrl){
     var Httpreq = new XMLHttpRequest(); // a new request
     Httpreq.open("GET",yourUrl,false);
