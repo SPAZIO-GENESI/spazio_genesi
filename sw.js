83391e62718f5181b66085ec8036a1f7b826d348
maxAlone <mascam64@gmail.com>
2025-11-08 17:51:57 +0100

    Update Content Security Policy to include new sources and increment cache version to v2.4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'static-cache-ottobre-v2.3'; // Aggiungi un numero di versione
+const CACHE_NAME = 'static-cache-ottobre-v2.4'; // Aggiungi un numero di versione
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
