b552932170350f6c4ce19232575f89ec46763c01
maxAlone <mascam64@gmail.com>
2025-11-16 14:33:32 +0100

    Update service worker cache name to 'static-cache-novembre-v1'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'static-cache-ottobre-v3.1'; // Aggiungi un numero di versione
+const CACHE_NAME = 'static-cache-novembre-v1; // Aggiungi un numero di versione
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
