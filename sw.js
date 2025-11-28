089773c4fd2dcdce8309aefede3499b3f6dc5cf4
maxAlone <mascam64@gmail.com>
2025-11-28 18:26:56 +0100

    milena 2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'static-cache-novembre-v15; // Aggiungi un numero di versione
+const CACHE_NAME = 'static-cache-novembre-v16; // Aggiungi un numero di versione
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
