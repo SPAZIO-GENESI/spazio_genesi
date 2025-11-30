0c570784989ca738e2c78e01ea1827caedb5a8fe
maxAlone <mascam64@gmail.com>
2025-11-30 17:37:53 +0100

    home eff e manu details

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'static-cache-novembre-v26; // Aggiungi un numero di versione
+const CACHE_NAME = 'static-cache-novembre-v27; // Aggiungi un numero di versione
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
