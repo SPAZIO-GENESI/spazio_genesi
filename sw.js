9ebe06d1744f375b367ec43bbf5865731454a8b4
maxAlone <mascam64@gmail.com>
2025-12-16 09:50:09 +0100

    su method

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'static-cache-dicembre-v5'; // Aggiungi un numero di versione
+const CACHE_NAME = 'static-cache-dicembre-v6'; // Aggiungi un numero di versione
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
