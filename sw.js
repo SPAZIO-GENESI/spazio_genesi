a18973c712c472b35a688e5dd35ceadc5703e825
maxAlone <mascam64@gmail.com>
2025-12-17 11:53:36 +0100

    csp def new

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'static-cache-dicembre-v15'; // Aggiungi un numero di versione
+const CACHE_NAME = 'static-cache-dicembre-v16'; // Aggiungi un numero di versione
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
