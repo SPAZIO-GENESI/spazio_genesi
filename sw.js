ddde554b4031228086070b4fc02f8c032f220af3
maxAlone <mascam64@gmail.com>
2025-12-17 11:49:04 +0100

    font tawk csp

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'static-cache-dicembre-v13'; // Aggiungi un numero di versione
+const CACHE_NAME = 'static-cache-dicembre-v14'; // Aggiungi un numero di versione
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
