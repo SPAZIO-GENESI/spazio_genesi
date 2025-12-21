c50f6894307995873641897fa50e538dba86d989
maxAlone <mascam64@gmail.com>
2025-12-21 13:04:37 +0100

    deb bio 2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'static-cache-dicembre-v18'; // Aggiungi un numero di versione
+const CACHE_NAME = 'static-cache-dicembre-v19'; // Aggiungi un numero di versione
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
