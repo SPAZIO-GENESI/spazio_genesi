3080ac5c91a5c490b4be50f0a968bf673c7a5eed
maxAlone <mascam64@gmail.com>
2026-01-14 15:08:38 +0100

    amoreodio

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'static-cache-gennaio-v1'; // Aggiungi un numero di versione
+const CACHE_NAME = 'static-cache-gennaio-v2'; // Aggiungi un numero di versione
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
