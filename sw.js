48220c9937ea1febe7763e8939d1fbfc963aeed2
maxAlone <mascam64@gmail.com>
2026-02-07 22:51:26 +0100

    nevoa portfolio

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'static-cache-febbraio-v1'; // Aggiungi un numero di versione
+const CACHE_NAME = 'static-cache-febbraio-v2'; // Aggiungi un numero di versione
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
