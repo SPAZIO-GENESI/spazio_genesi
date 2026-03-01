41991fe9ab09ea62071ae1f455387fe0aec7970b
maxAlone <mascam64@gmail.com>
2026-03-01 11:14:13 +0100

    turnstile light

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'static-cache-febbraio-v10'; // Aggiungi un numero di versione
+const CACHE_NAME = 'static-cache-marzo-v1'; // Aggiungi un numero di versione
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
