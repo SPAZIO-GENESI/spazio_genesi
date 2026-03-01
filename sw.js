d79a933ee328cf1337b6782f1f7f76dc77750790
maxAlone <mascam64@gmail.com>
2026-03-01 11:23:04 +0100

    condizioni corso

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'static-cache-marzo-v1'; // Aggiungi un numero di versione
+const CACHE_NAME = 'static-cache-marzo-v2'; // Aggiungi un numero di versione
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
