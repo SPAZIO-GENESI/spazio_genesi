2d29b65ae84b5dfef36b29d82eeef7920b28ff52
maxAlone <mascam64@gmail.com>
2026-03-11 09:20:28 +0100

    intinerari

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'static-cache-marzo-v8'; // Aggiungi un numero di versione
+const CACHE_NAME = 'static-cache-marzo-v9'; // Aggiungi un numero di versione
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
